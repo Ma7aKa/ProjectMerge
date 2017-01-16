1d4843624233b4274cfbe8aba9acb3c502d9c386
Oleg Babenko <oleg215_97@bk.ru>
2017-01-16 11:04:54 +0200

    Program.java

--- Program.java
+++ Program.java
@@ -2,14 +2,29 @@ import java.util.Scanner;
 
 public class Program
 {
+	private Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String [] args)
 	{
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hello");
+		Program program = new Program();
+		program.start();
 	}
 	
+	public void start()
+	{
+		int a = input("First number: ");
+		int b = input("Second number: ");
+		System.out.println(sum(a,b));
+	}
 	public int sum(int a,int b)
 	{
 		return a + b;
+	}
+	
+	public int input(String text)
+	{
+		System.out.print(text + ": ");
+		return scanner.nextInt();
 	}
 }
